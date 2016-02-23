afc8548094edcfc7288f124b8b7a8b1f94556208
Shayan123456 <shayan_jon123@yahoo.com>
2016-02-23 21:46:30 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -229,11 +229,15 @@ function create_config( )
     "anti_spam",
     "add_bot",
     "owners",
-    "chatbot",
     "set",
+    "chatbot",
+    "sendplug",
+    "emojilock",
+    "lockfosh",
     "location",
     "plugins",
     "get",
+    "info",
     "leave",
     "wiki",
     "translate",
@@ -248,7 +252,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {87160007,137791771},--Sudo users
+    sudo_users = {87160007},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
